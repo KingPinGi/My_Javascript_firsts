88ee479313af996dca1655a718293da525cacb92
Oluwatobiloba Aina <ainatobi83gmail.com>
2022-09-01 20:46:27 -1100

    all files

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,6 +61,8 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 const displayMovements = function(movements){
   containerMovements.innerHTML = '';
 
@@ -81,8 +83,79 @@ const displayMovements = function(movements){
 };
 displayMovements(account1.movements);
 
+const createUsername = function(acc){
 
-console.log(containerMovements.innerHTML);
+  accounts.forEach(function(acc){
+    acc.username = acc.owner
+    .toLowerCase()
+    .split(' ')
+    .map(name => name[0])
+    .join('');
+  })
+};
+createUsername(accounts);
+console.log(accounts);
+
+const deposits = movements.filter(function(mov){
+  return mov > 0;
+})
+console.log(movements);
+console.log(deposits)
+
+const withdrawal = movements.filter(function(mov){
+  return mov < 0;
+});
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawal);
+console.log(withdrawals);
+
+
+
+const user = 'Steven Thomas Williams';
+ 
+const calcDisplayBlance = function(movements){
+  const max = movements.reduce(acc, nov);
+}
+ 
+
+
+// console.log(containerMovements.innerHTML);
+// Coding Challenge #1
+
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+//MY SOLUTION
+//THE DATA
+// const dogsJulia = [3,5,2,12,7];
+// const dogsKate = [4, 1, 15, 8, 3];
+
+// const newDogsKatie = dogsJulia.slice(1, -2);
+// const allDogs = [...newDogsKatie, ...dogsKate];
+
+// allDogs.forEach(function(dogs, i, arr){
+//   if (dogs >= 3){
+//     console.log(`Dog number ${i + 1} is an adult and is ${dogs} years old`);
+//   }else{
+//     console.log(`Dog number ${i + 1} is a puppy`);
+//   }
+// }
+// )
+
 
 
 /////////////////////////////////////////////////
@@ -95,7 +168,7 @@ console.log(containerMovements.innerHTML);
 //   ['GBP', 'Pound sterling'],
 // ]);
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
 
 /*
 /////////////////////////////////////////////////
@@ -147,7 +220,7 @@ movements.forEach(function(mov, i, arr){
 
 //WE cant break out of a foreach loop, the continue and break statements do not work 
 //in a foreach method
-*/
+
 
 //foreach loop with maps and sets
 
@@ -168,3 +241,69 @@ console.log(currenciesUnique);
 currenciesUnique.forEach(function(value, key, map){
   console.log(`${ value}: ${value}`);
 })
+
+const eurToUsd = 1.1;
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+// const movementsUSD = movements.map(function(mov){
+//   return Math.trunc(mov * eurToUsd);
+// });
+
+
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+console.log(movementsUSDfor);
+
+const movementsDescription = movements.map((mov, i ) => {
+
+  `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(mov)}` 
+
+  
+}); 
+console.log(movementsDescription);
+*/
+
+// Coding Challenge #2
+
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+const calcAverageHumanAge = function (ages) {
+  const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  const adults = humanAges.filter(age => age >= 18);
+  console.log(humanAges);
+  console.log(adults);
+
+  // const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+
+  const average = adults.reduce(
+    (acc, age, i, arr) => acc + age / arr.length,
+    0
+  );
+
+  // 2 3. (2+3)/2 = 2.5 === 2/2+3/2 = 2.5
+
+  return average;
+};
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2); 
+
+const eurToUsd = 1.1;
+const totalDepositUSD = movements.filter(mov => mov > 0).map(mov => mov * eurToUsd).reduce((acc, mov) => acc + mov, 0) 
+console.log(totalDepositUSD);
