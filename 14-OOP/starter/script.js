1059e91e9fa08bbf3dff64aee883cc22175cac30
Oluwatobiloba Aina <ainatobi83@gmail.com>
2022-09-08 08:24:45 +0100

    oop commit

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const Person = function(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+
+    this.calcAge = function(){
+        return 2037 - this.birthYear
+    }
+};
+
+const jonas = new Person('jonas', 1991);
+console.log(jonas);
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('jack', 1967);
+console.log(matilda, jack);
+
+console.log(jack instanceof Person);
+
+//Prototype
+Person.prototype.calcAge = function(){
+    console.log(2037 - this.birthYear);
+};
+console.log(jonas);
+
+matilda.calcAge();
+jonas.calcAge();
