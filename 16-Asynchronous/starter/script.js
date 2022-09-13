f664277f9401b8d539c337fcedf7230519a5425d
KingPinGi <ainatobi83@gmail.com>
2022-09-13 15:36:55 +0100

    error commit

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -3,6 +3,28 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderCountry = function(data, className ='' ){
+    const html = `
+<article class="country ${className}">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+     <h3 class="country__name">${data.name.common}</h3>
+     <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(+data.population/ 1000000).toFixed(1)} people</p>
+     <p class="country__row"><span>🗣️</span>${Object.values(data.languages)[0]}</p>
+     <p class="country__row"><span>💰</span>${Object.values(data.currencies)[0].name}</p>
+  </div>
+</article>
+`;
+countriesContainer.insertAdjacentHTML('beforeend', html);
+countriesContainer.style.opacity = 1;
+};
+
+const renderError = function(msg){
+    countriesContainer.insertAdjacentText('beforeend', msg);
+    countriesContainer.style.opacity = 1;
+}
+
 ///////////////////////////////////////
 
 // const getCountryData = function(country) {
@@ -38,24 +60,6 @@ const countriesContainer = document.querySelector('.countries');
 // getCountryData('portugal');
 // getCountryData('usa');
 // getCountryData('germany');
-
-
-const renderCountry = function(data, className ='' ){
-    const html = `
-<article class="country ${className}">
-  <img class="country__img" src="${data.flags.png}" />
-  <div class="country__data">
-     <h3 class="country__name">${data.name.common}</h3>
-     <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(+data.population/ 1000000).toFixed(1)} people</p>
-     <p class="country__row"><span>🗣️</span>${Object.values(data.languages)[0]}</p>
-     <p class="country__row"><span>💰</span>${Object.values(data.currencies)[0].name}</p>
-  </div>
-</article>
-`;
-countriesContainer.insertAdjacentHTML('beforeend', html);
-countriesContainer.style.opacity = 1;
-};
 
 /*
 const getCountryAndNeighbour = function(country) {
@@ -113,11 +117,14 @@ setTimeout(() => {
 // const request = fetch(`https://restcountries.com/v3.1/name/portugal`);
 // console.log(request);
 
+
 const getCountryData = function(country){
     //country 1
     fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then(response => response.json())
     .then(data => {
+        console.log(data);
+        renderCountry(data[0]);
         const neighbour = data[0].borders[0];
 
         if(!neighbour) return;
@@ -128,6 +135,17 @@ const getCountryData = function(country){
     .then(response => response.json())
     .then(data => {
         renderCountry(data[0], 'neighbour');
+    })
+    .catch(err => {
+        console.log(`${err} 💥💢💢💥`);
+        renderError(`Something went wrong ${err.message}, Try again!!`);
+    })
+    .finally(() => {
+        countriesContainer.style.opacity = 1;
     });
 };
-getCountryData('usa');
+
+btn.addEventListener('click', function(){
+    getCountryData('usa');
+})
+getCountryData('tobi');
