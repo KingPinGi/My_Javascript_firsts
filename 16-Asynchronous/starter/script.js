6deff8ddffa369b7ff88b9c47ab55afb6fa872e0
KingPinGi <ainatobi83@gmail.com>
2022-09-13 14:44:34 +0100

    request commit

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -118,15 +118,16 @@ const getCountryData = function(country){
     fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then(response => response.json())
     .then(data => {
-        renderCountry(data[0]);
-        console.log(data[0]);
-        console.log(data[0].borders[0]);
         const neighbour = data[0].borders[0];
 
         if(!neighbour) return;
 
         //country 2
-        fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+        return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => {
+        renderCountry(data[0], 'neighbour');
     });
 };
 getCountryData('usa');
