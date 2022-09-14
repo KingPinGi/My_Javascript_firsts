bd802b21cf626576b165d29c49937486674d35fa
KingPinGi <ainatobi83@gmail.com>
2022-09-14 15:00:02 +0100

    maps commit

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -117,35 +117,169 @@ setTimeout(() => {
 // const request = fetch(`https://restcountries.com/v3.1/name/portugal`);
 // console.log(request);
 
+const getJSON = function(url, errorMsg = 'Something went wrong'){
+    return fetch(url).then(response => { 
+        if(!response.ok){
+        throw new Error(`${errorMsg} (${response.status})`)
+    }
+    return response.json();
+});
+}
+
+// const getCountryData = function(country){
+//     //country 1
+//     fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(response => {
+//         console.log(response);
+
+//         if(!response.ok){
+//             throw new Error(`Country not found (${response.status})`)
+//         }
+//         return response.json()
+//     })
+//     .then(data => {
+//         console.log(data);
+//         renderCountry(data[0]);
+//         // const neighbour = data[0].borders[0];
+//         const neighbour = tobi;
+
+//         if(!neighbour) return;
+
+//         //country 2
+//         return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+//     })
+//     .then(response => {
+    // if(!response.ok)
+    //   throw new Error(`Country not found(${response.status})`)
+//      return response.json()})
+//     .then(data => {
+//         renderCountry(data[0], 'neighbour');
+//     })
+//     .catch(err => {
+//         console.log(`${err} 💥💢💢💥`);
+//         renderError(`Something went wrong ${err.message}, Try again!!`);
+//     })
+//     .finally(() => {
+//         countriesContainer.style.opacity = 1;
+//     });
+// };
+
+
 
 const getCountryData = function(country){
-    //country 1
-    fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        renderCountry(data[0]);
-        const neighbour = data[0].borders[0];
+        //country 1
+        getJSON(`https://restcountries.com/v3.1/name/${country}`, 'Country not found')
 
-        if(!neighbour) return;
-
-        //country 2
-        return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
-    })
-    .then(response => response.json())
-    .then(data => {
-        renderCountry(data[0], 'neighbour');
-    })
-    .catch(err => {
-        console.log(`${err} 💥💢💢💥`);
-        renderError(`Something went wrong ${err.message}, Try again!!`);
-    })
-    .finally(() => {
-        countriesContainer.style.opacity = 1;
-    });
-};
+        .then(data => {
+            console.log(data[0]);
+            renderCountry(data[0]);
+            const neighbour = data[0].borders[0];
+    
+            if (!neighbour) {
+                throw new Error('No neighbour found');
+                
+            }
+            
+    
+            //country 2
+            return getJSON(`https://restcountries.com/v3.1/alpha/${neighbour}`, 'Country not found');
+        })
+        .then(data => {
+            renderCountry(data[0], 'neighbour');
+        })
+        .catch(err => {
+            console.log(`${err.message} 💥💢💢💥`);
+            renderError(`Something went wrong ${err.message}, Try again!`);
+        })
+        .finally(() => {
+            countriesContainer.style.opacity = 1;
+        });
+    };
 
 btn.addEventListener('click', function(){
-    getCountryData('usa');
+        getCountryData('usa');
+});
+getCountryData('australia');
+
+///////////////////////////////////////
+// Coding Challenge #1
+/* 
+In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. For that, you will use a second API to geocode coordinates.
+Here are your tasks:
+PART 1
+1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
+2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://geocode.xyz/api.
+The AJAX call will be done to a URL with this format: https://geocode.xyz/52.508,13.381?geoit=json. Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
+3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided location. Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
+4. Chain a .catch method to the end of the promise chain and log errors to the console
+5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does NOT reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message.
+
+PART 2
+6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
+7. Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
+
+TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
+TEST COORDINATES 2: 19.037, 72.873
+TEST COORDINATES 2: -33.933, 18.474
+
+GOOD LUCK 😀
+*/
+
+/*
+javascript only has one thread
+it can only do a thing at a time
+there is absolutely no multitasking
+Java can execute multiple pieces of codes but not js
+Callback Queue
+ready-to-be executed callback function
+
+EVENT LOOPS
+One of the reasons that make asynchronous js to work
+
+if js works on only one thread how can it work in an asynchronous way and how is asynchronous code exxecuted
+
+for asynchronous tasks, they all run in the web api of the browser
+loading images in js is async
+
+listen to the load event to work a callback function after the load
+the callback wont run until the image is loaded
+
+ajax call using fetch
+Callback Queue is basically an ordered list of thr callbacks function in line to be executed
+a to-do list, a kind
+tasks the call stack would have to execute
+
+EVENT LOOP
+it looks into ksthe call stack and checks if its empty and if yes it takes a callback from the callback queue, 
+it decides when each callback is executed: orchestration
+js has no sense of time
+
+Event loop decides whuch code is executed next
+event loop picks u code from the callback queue and puts thm in the call stack
+microtasks queue, more priority, microtasks   promises
+
+console.log('Test start');
+setTimeout(() => console.log('0 sec timer'), 0);
+//Allows us to create a promise that is easily resolved
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+Promise.resolve('Resolved promise 2').then(res => {
+    for (let i = 0; i < 100000; i++){}
+    console.log(res);
 })
-getCountryData('tobi');
+
+console.log('Test end');
+*/
+
+const lotteryPromise = new Promise(function(resolve, reject){
+    console.log('Lottery draw is happening');
+    setTimeout(function(){
+    if(Math.random() >= 0.5){
+        resolve('YOU WIN')
+    }else{
+        reject(new Error('You have lost your money'));
+    }
+    }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.log(err));
