b228f8958a4512395f63013b5290a4aec28cae2d
KingPinGi <ainatobi83@gmail.com>
2022-09-15 23:35:51 +0100

    more commits

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -24,7 +24,8 @@
 
 // console.log(cart);
 
-(function(){
+/*
+const ShoppingCart2 = (function(){
     const cart = [];
     const shippingCost = 10;
     const totalPrice = 237;
@@ -32,10 +33,32 @@
 
     const addToCart = function(product, quantity){
         cart.push({product, quantity});
-        console.log(`${quantity} ${product} added to cart`);
+        console.log(`${quantity} ${product} added to cart (shipping cost is ${shippingCost})`);
     };
 
-    const addToCart = function(product, quantity){
-        console.log(`${quantity} $`)
-    }
-})
+    const orderStock = function(product, quantity){
+        console.log(`${quantity} ${product} ordered from supplier`);
+    };
+
+    return{
+        addToCart,
+        cart,
+        totalQuantity,
+        totalPrice,
+        shippingCost
+    };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+*/
+
+//Export
+export.addToCart = function(product, quantity){
+    cart.push({product, quantity});
+    console.log(`${quantity} ${product} added to cart (shipping cost is ${shippingCost})`);
+};
+
+//Import
+const { addToCart } = require('./shoppingCart.js');
